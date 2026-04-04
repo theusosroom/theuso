573632c608d91d9f20ca3ea8c87f4d03646e3093
theusosroom <matheus.m2008@gmail.com>
2026-04-04 00:24:26 +0000

    centralizado logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,10 @@ import Link from "next/link";
 export function Navbar() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-slate-300/70 backdrop-blur-md">
-      <nav className="container mx-auto flex h-16 items-center justify-between px-4">
+      <nav className="container mx-auto flex h-16 items-center justify-center px-4">
         <Link href="/" className="text-xl font-bold tracking-tighter text-blue-600">
           Theuso.
         </Link>
-
-        
       </nav>
     </header>
   );
